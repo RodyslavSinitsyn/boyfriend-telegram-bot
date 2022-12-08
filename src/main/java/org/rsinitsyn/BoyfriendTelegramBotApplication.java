d478ff8ea908b492ffb48507c953852073545ead
Rodyslav Sinitsyn <r.sinitsyn@elsevier.com>
2022-12-08 15:46:43 +0200

    Initial commit

---
+++ src/main/java/org/rsinitsyn/BoyfriendTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package org.rsinitsyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoyfriendTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoyfriendTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rsinitsyn/BoyfriendTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rsinitsyn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoyfriendTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
